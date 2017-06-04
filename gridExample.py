379323a3c757c69cb0fb4c4221fd64f5763a46de
Suslanger <ilyabelick@gmail.com>
2017-06-04 17:22:57 +0300

    Update gridExample.py

--- gridExample.py
+++ gridExample.py
@@ -1,11 +1,7 @@
 import sys
 from tkinter import *
 
-myApp = Tk()
-myApp.title('My App')
-
-myApp.geometry('400x400+350+340')
-
+myApp = Tk(title = "My App", geometry = 400x400+350+340)
 label = Label(text='My Label', fg ='red', bg = 'white').grid(row=0,column=0,sticky=E)
 
 myApp.mainloop()
